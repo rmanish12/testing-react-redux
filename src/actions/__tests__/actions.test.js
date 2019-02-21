e3fa1108614a9f47133b9e6228c1076d2d9e436e
Manish <manish@Manishs-MacBook-Air.local>
2019-02-21 20:43:19 +0530

    third commit

---
+++ src/actions/__tests__/actions.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { SAVE_COMMENT } from '../types'
+import { saveComment } from '../index'
+
+describe('save Comment', () => {
+    it('has the correct type', () => {
+        const action = saveComment()
+
+        expect(action.type).toEqual(SAVE_COMMENT)
+    })
+
+    it('has the correct payload', () => {
+        const action = saveComment('new comment')
+
+        expect(action.payload).toEqual('new comment')
+    })
+})
